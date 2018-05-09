4fce0e11d9899330d2665c62b4965f0aa9946e80
mshiloh <mshiloh.code@gmail.com>
2018-05-09 14:50:36 -0600

    added image to card schema

--- models/cardModel.js
+++ models/cardModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const cardSchema = new Schema({
+    image: {
+        type: String,
+        required: true
+    },
     color: {
         type: String,
         required: true
