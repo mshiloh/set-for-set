32e58b7237f9fa88086f523bf0dbf6decec7b62b
mshiloh <mshiloh.code@gmail.com>
2018-05-14 15:08:24 -0600

    rendered cards

---
+++ client/src/App/GamePlay/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+
+import {getCards} from "../../redux/cards";
+
+import GameComponent from "./GameComponent";
+
+class GamePlay extends Component {
+    componentDidMount(){
+        this.props.getCards();
+    }
+    render() {
+        const {data} = this.props;
+        const dealtCards = data.map((card, i) => <GameComponent key={card + i} {...card}></GameComponent>)
+        return (
+            <div>
+                {dealtCards}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return state.cards
+}
+
+export default connect(mapStateToProps, { getCards })(GamePlay);
