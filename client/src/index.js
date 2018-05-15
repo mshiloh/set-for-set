0fbbdc06d4afad30886cce9cba84d0bf61e1f553
mshiloh <mshiloh.code@gmail.com>
2018-05-15 17:13:00 -0600

    made login button responsive

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./style.css";
+
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/index.js";
