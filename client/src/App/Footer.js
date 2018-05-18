a78953c156e41c6557f93967a5cdbc6e18ed0f84
mshiloh <mshiloh.code@gmail.com>
2018-05-18 16:54:41 -0600

    post

--- client/src/App/Footer.js
+++ client/src/App/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(props) {
     return (
         <footer className="footer-wrapper">
-            {/* <p>Melting Pot Team online rendition of the SET game © 2018</p> */}
+            <p>Ilian | Anik | Morgan | The Melting Pot Team © 2018</p>
             <p className="copyright">Copyright © 1998, 1991 Cannei, LLC. All rights reserved. SET® and all designated logos and slogans are registered trademarks of Cannei, LLC. </p>
         </footer>
     )
